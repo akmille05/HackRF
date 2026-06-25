9e32d73cfa1de646d20f6ef5ba71390ef4fc970c
alyssamiller95 <mille9ak@dukes.jmu.edu>
2026-06-25 14:10:28 -0400

    new

--- basicHackRF.py
+++ basicHackRF.py
@@ -9,7 +9,7 @@ class HackRF:
     sample_rate: int
     LNA_gain: int
     VGA_gain: int
-    RF_amplify_enable: int
+    RF_amplify_enable: bool
 
     def __init__(self):
         self.sdr = pyhackrf.pyhackrf_open() 
@@ -28,22 +28,6 @@ class HackRF:
         line = f"Board ID: {board_ID}, Model: {model_name}, Version: {version}, Serial: {serial}"
         return line
         
-    # def send_command(self,command):
-    #     if command  == "version":
-    #         return self.sdr.pyhackrf_version_string_read()
-    #     elif command == "serial":
-    #         return self.sdr.pyhackrf_board_partid_serialno_read()
-    #     elif command == "board_ID":
-    #         return self.sdr.pyhackrf_board_id_read()[0]
-    #     elif command == "model_name":
-    #         return self.sdr.pyhackrf_board_id_read()[1]
-    #     else: 
-    #         return "Unknown command"
-
-    # def query_result(self):
-    #     result = self.send_command("getfreq")
-    #     line = f"Query result: {result}"
-    #     return line
     def __del__(self):
         self.sdr.pyhackrf_close()
 
@@ -53,25 +37,13 @@ class HackRF:
         return self.frequency
 
     def setFrequency(self, frequency):
-        self.send_command("frequency")
+        self.frequency = frequency
 
     def getSampleRate(self):
         return self.sample_rate
 
     def setSampleRate(self, sample_rate):
-        self.send_command("sample_rate")
-
-    # def getLNA_gain(self):
-    #     return self.LNA_gain
-
-    # def setLNA_gain(self, LNA_gain):
-    #     self.send_command("LNA_gain")
-
-    # def getVGA_gain(self):
-    #     return self.VGA_gain
-
-    # def setVGA_gain(self, VGA_gain):
-    #     self.send_command("VGA_gain")
+        self.sample_rate = sample_rate
 
     def getRF_amplify_enable(self):
         return self.RF_amplify_enable
