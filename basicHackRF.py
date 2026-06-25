085a12f6db407e0f0cdcdb6ceaea2f0addcb2a45
alyssamiller95 <mille9ak@dukes.jmu.edu>
2026-06-25 09:27:43 -0400

    update

--- basicHackRF.py
+++ basicHackRF.py
@@ -1,30 +1,74 @@
 from python_hackrf import pyhackrf
 pyhackrf.pyhackrf_init()
 sdr = pyhackrf.pyhackrf_open() 
-# config variables go here
-#sdr
 
+class HackRF:
 # find out what the config variables are
-
-
-def devInfo():
-
-    board_id = pyhackrf.pyhackrf_board_id_read(sdr)
-    version = pyhackrf.pyhackrf_version_string_read(sdr)
-    serial = pyhackrf.pyhackrf_board_partid_serialno_read(sdr)
-    print(f"Board ID: {board_id}, Version: {version}, Serial: {serial}")
     
-def send_command(command):
-    if command  == "version":
-        return pyhackrf.pyhackrf_version_string_read(sdr)
-    elif command == "serial":
-        return pyhackrf.pyhackrf_board_partid_serialno_read(sdr)
-    elif command == "board_id":
-        return pyhackrf.pyhackrf_board_id_read(sdr)
-    else: 
-        return "Unknown command"
+    frequency
+    sample_rate
+    LNA_gain
+    VGA_gain
+    RF_amplify_enable
 
-def query_result():
-    
+    def __init__(self):
+        self.frequency = 0
+        self.sample_rate = 0
+        self.LNA_gain = 0
+        self.VGA_gain = 0  
+        self.RF_amplify_enable = False
 
-# Write getters and setters for the config variables
+
+    def devInfo(self):
+
+        board_ID, model_name = sdr.pyhackrf_board_id_read()
+        version = sdr.pyhackrf_version_string_read()
+        serial = sdr.pyhackrf_board_partid_serialno_read()
+        print(f"Board ID: {board_ID}, Model: {model_name}, Version: {version}, Serial: {serial}")
+        
+    def send_command(self,command):
+        if command  == "version":
+            return sdr.pyhackrf_version_string_read()
+        elif command == "serial":
+            return sdr.pyhackrf_board_partid_serialno_read()
+        elif command == "board_ID":
+            return sdr.pyhackrf_board_id_read()[0]
+        elif command == "model_name":
+            return sdr.pyhackrf_board_id_read()[1]
+        else: 
+            return "Unknown command"
+
+    def query_result(self,result):
+        print(f"Query result: {result}")
+
+    # Write getters and setters for the config variables
+
+    def getFrequency(self)
+        return self.frequency
+
+    def setFrequency(self, frequency):
+        self.frequency = frequency
+
+    def getSampleRate(self):
+        return self.sample_rate
+
+    def setSampleRate(self, sample_rate):
+        self.sample_rate = sample_rate
+
+    def getLNA_gain(self):
+        return self.LNA_gain
+
+    def setLNA_gain(self, LNA_gain):
+        self.LNA_gain = LNA_gain
+
+    def getVGA_gain(self):
+        return self.VGA_gain
+
+    def setVGA_gain(self, VGA_gain):
+        self.VGA_gain = VGA_gain
+
+    def getRF_amplify_enable(self):
+        return self.RF_amplify_enable
+
+    def setRF_amplify_enable(self, RF_amplify_enable):
+        self.RF_amplify_enable = RF_amplify_enable
