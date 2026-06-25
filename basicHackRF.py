9a68801f999914ead68366e44d0c0944e5667a6c
alyssamiller95 <mille9ak@dukes.jmu.edu>
2026-06-25 11:37:59 -0400

    changelog

--- basicHackRF.py
+++ basicHackRF.py
@@ -1,45 +1,51 @@
 from python_hackrf import pyhackrf
 pyhackrf.pyhackrf_init()
-sdr = pyhackrf.pyhackrf_open() 
+
 
 class HackRF:
-# find out what the config variables are
-    
-    frequency : int
-    sample_rate : int
-    LNA_gain : int
-    VGA_gain : int
-    RF_amplify_enable : int
+
+    sdr: pyhackrf
+    frequency: int
+    sample_rate: int
+    LNA_gain: int
+    VGA_gain: int
+    RF_amplify_enable: int
 
     def __init__(self):
-        self.frequency = 0
+        self.sdr = pyhackrf.pyhackrf_open() 
+        self.frequency = 100e6
         self.sample_rate = 0
-        self.LNA_gain = 0
-        self.VGA_gain = 0  
+        # self.LNA_gain = 0
+        # self.VGA_gain = 0  
         self.RF_amplify_enable = False
-
 
     def devInfo(self):
 
-        board_ID, model_name = sdr.pyhackrf_board_id_read()
-        version = sdr.pyhackrf_version_string_read()
-        serial = sdr.pyhackrf_board_partid_serialno_read()
-        print(f"Board ID: {board_ID}, Model: {model_name}, Version: {version}, Serial: {serial}")
-        
-    def send_command(self,command):
-        if command  == "version":
-            return sdr.pyhackrf_version_string_read()
-        elif command == "serial":
-            return sdr.pyhackrf_board_partid_serialno_read()
-        elif command == "board_ID":
-            return sdr.pyhackrf_board_id_read()[0]
-        elif command == "model_name":
-            return sdr.pyhackrf_board_id_read()[1]
-        else: 
-            return "Unknown command"
+        board_ID, model_name = self.sdr.pyhackrf_board_id_read()
+        version = self.sdr.pyhackrf_version_string_read()
+        serial = self.sdr.pyhackrf_board_partid_serialno_read()
 
-    def query_result(self,result):
-        print(f"Query result: {result}")
+        line = f"Board ID: {board_ID}, Model: {model_name}, Version: {version}, Serial: {serial}"
+        return line
+        
+    # def send_command(self,command):
+    #     if command  == "version":
+    #         return self.sdr.pyhackrf_version_string_read()
+    #     elif command == "serial":
+    #         return self.sdr.pyhackrf_board_partid_serialno_read()
+    #     elif command == "board_ID":
+    #         return self.sdr.pyhackrf_board_id_read()[0]
+    #     elif command == "model_name":
+    #         return self.sdr.pyhackrf_board_id_read()[1]
+    #     else: 
+    #         return "Unknown command"
+
+    # def query_result(self):
+    #     result = self.send_command("getfreq")
+    #     line = f"Query result: {result}"
+    #     return line
+    def __del__(self):
+        self.sdr.pyhackrf_close()
 
     # Write getters and setters for the config variables
 
@@ -47,28 +53,28 @@ class HackRF:
         return self.frequency
 
     def setFrequency(self, frequency):
-        self.frequency = frequency
+        self.send_command("frequency")
 
     def getSampleRate(self):
         return self.sample_rate
 
     def setSampleRate(self, sample_rate):
-        self.sample_rate = sample_rate
+        self.send_command("sample_rate")
 
-    def getLNA_gain(self):
-        return self.LNA_gain
+    # def getLNA_gain(self):
+    #     return self.LNA_gain
 
-    def setLNA_gain(self, LNA_gain):
-        self.LNA_gain = LNA_gain
+    # def setLNA_gain(self, LNA_gain):
+    #     self.send_command("LNA_gain")
 
-    def getVGA_gain(self):
-        return self.VGA_gain
+    # def getVGA_gain(self):
+    #     return self.VGA_gain
 
-    def setVGA_gain(self, VGA_gain):
-        self.VGA_gain = VGA_gain
+    # def setVGA_gain(self, VGA_gain):
+    #     self.send_command("VGA_gain")
 
     def getRF_amplify_enable(self):
         return self.RF_amplify_enable
 
     def setRF_amplify_enable(self, RF_amplify_enable):
-        self.RF_amplify_enable = RF_amplify_enable
+        self.sdr.pyhackrf_set_antenna_enable(bool)
