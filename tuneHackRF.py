4622eeaabfe2dc08d008ac85fe337fc61c23f0c2
Addison Edwards <edwardsaddison26@gmail.com>
2026-07-06 09:17:52 -0400

    edited tunehackrf script to allow for recieving FM radio

--- tuneHackRF.py
+++ tuneHackRF.py
@@ -9,15 +9,38 @@ HackRF.
 from python_hackrf import pyhackrf
 from basicHackRF import HackRF
 import time
+import numpy as np
 
 pyhackrf.pyhackrf_init()
 
-def rx_callback(transfer):
+callback_count = 0
+
+def rx_callback(device, buffer, buffer_length, valid_length):
     """
     Runs automatically whenever the HackRF receives samples.
     """
-    print("Received samples")
+
+    raw = buffer[:valid_length]
+
+    i_samples = raw[0::2]
+    q_samples = raw[1::2]
+
+    iq_samples = i_samples + 1j * q_samples
+
+    audio = demodulate_fm(iq_samples)
+
+    print("Audio samples:", len(audio))
+
     return 0
+
+def demodulate_fm(iq_samples):
+    """
+    Convert IQ samples into FM audio samples.
+    """
+
+    angles = np.angle(iq_samples[1:] * np.conj(iq_samples[:-1]))
+
+    return angles
 
 def main():
     # Connect to the HackRF
@@ -35,14 +58,21 @@ def main():
     sdr.sdr.set_rx_callback(rx_callback) #tells the HackRF which function to call
     sdr.sdr.pyhackrf_start_rx() #starts receiving
 
-    print("Receiving for 5 seconds...")
-    time.sleep(5)
+    #print("Receiving for 5 seconds...")
+    #time.sleep(5)
+
+    print("Receiving samples...")
+    print("Press Ctrl + C to stop.")
+
+    try:
+        while True:
+            time.sleep(1)
+
+    except KeyboardInterrupt: #Ctrl C stops the loop
+        print("Stopping receive...")
 
     sdr.sdr.pyhackrf_stop_rx() #stops receiving
     print("Stopped receiving.")
-
-    # Demodulate FM
-    # ...
 
     # Play the audio
     # ...
